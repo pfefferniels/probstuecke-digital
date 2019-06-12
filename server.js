6472971d527ec703e84137cac098f3cc6e99cef4
pfefferniels <niels.pfeffer@gmail.com>
2019-06-12 09:02:04 +0200

    fix

--- server.js
+++ server.js
@@ -385,6 +385,6 @@ app.get('/description', function(req, res) {
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log('Listening on port ' + port);
+  console.log('Listening');
 });
 
