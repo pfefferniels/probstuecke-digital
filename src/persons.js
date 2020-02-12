157f8d81b07999fc1cc2f918ed6b184e74a5f715
pfefferniels <niels.pfeffer@gmail.com>
2020-02-12 09:42:08 +0100

    improve index of persons

--- src/persons.js
+++ src/persons.js
@@ -5,12 +5,18 @@ const persons = require('express').Router(),
 
 persons.get('/', function(req, res) {
   db.queries.readAll(
-    `xquery version "3.1";
-     declare namespace tei="http://www.tei-c.org/ns/1.0";
-     collection('/db/apps/probstuecke-digital/')//tei:body//tei:persName`, {})
+    `
+    xquery version "3.1";
+    declare namespace tei="http://www.tei-c.org/ns/1.0";
+    for $persName in collection('/db/apps/probstuecke-digital/')//tei:body//tei:persName[@ref]
+    let $place := <location>{substring-after(base-uri($persName), "probstuecke-digital/")}</location>
+    order by $persName/@ref
+    group by $persName
+    return <person>{$persName}<locations>{$place}</locations></person>
+    `, {})
     .then(function (result) {
       res.render('persons', {
-        persons: Buffer.concat(result.pages).toString()
+        persons: '<persons>' + Buffer.concat(result.pages).toString() + '</persons>'
       });
     })
     .catch(e => console.error(e));
