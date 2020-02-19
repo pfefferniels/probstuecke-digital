716e4dca3a9f1560f67c683f4d2215677c813fd3
pfefferniels <niels.pfeffer@gmail.com>
2020-02-19 20:18:50 +0100

    remove unused code

--- src/parseMEI.js
+++ src/parseMEI.js
@@ -8,44 +8,6 @@ const exist = require('@existdb/node-exist'),
       PDFDocument = require('pdfkit'),
       SVGtoPDF = require('svg-to-pdfkit');
 
-// takes a DOM object and replaces all <clef>s and <staffDef>s with
-// modern clefs
-function modernizeClefs(doc) {
-  const replacementMap = {
-    "C1": {shape: "G", line: 2},
-    "C2": {shape: "G", line: 2},
-    "C3": {shape: "G", line: 2},
-    "C4": {shape: "F", line: 4},
-    "F3": {shape: "F", line: 4}
-  };
-
-  // find all <staffDef>s.
-  const staffDefs = doc.documentElement.getElementsByTagName("staffDef");
-  for (let i=0; i<staffDefs.length; i++) {
-    const shape = staffDefs[i].getAttribute("clef.shape");
-    const line = staffDefs[i].getAttribute("clef.line");
-    const comb = shape + line;
-    if (comb in replacementMap) {
-      const replacement = replacementMap[comb];
-      staffDefs[i].setAttribute("clef.line",  replacement.line);
-      staffDefs[i].setAttribute("clef.shape", replacement.shape);
-    }
-  }
-
-  // find all remaining clef changes
-  const clefs = doc.documentElement.getElementsByTagName("clef");
-  for (let i=0; i<clefs.length; i++) {
-    const shape = clefs[i].getAttribute("shape");
-    const line = clefs[i].getAttribute("line");
-    const comb = shape + line;
-    if (comb in replacementMap) {
-      const replacement = replacementMap[comb];
-      clefs[i].setAttribute("line",  replacement.line);
-      clefs[i].setAttribute("shape", replacement.shape);
-    }
-  }
-}
-
 function parseMEI(number, label, file, options) {
   let queryParams = {
     variables: {
