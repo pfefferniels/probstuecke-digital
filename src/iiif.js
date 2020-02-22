5879e1e64cade4412a3a452676f6fa93533365b0
pfefferniels <niels.pfeffer@gmail.com>
2020-02-22 11:39:55 +0100

    load facsimile annotation lists from DB instead of file system (using eXists REST API, since the XML-RPC API doesn't support loading non-XML documents)

--- src/iiif.js
+++ src/iiif.js
@@ -1,23 +1,31 @@
 // provide a simple AnnotationList service in the
 // recommended URI pattern: /{prefix}/{identifier}/list/{name}
 
-const path = require('path'),
+const axios = require('axios'),
+      existConfig = require('../existConfig.json'),
       iiif = require('express').Router();
 
 function sendAnnotationList(req, res) {
   if (isNaN(req.params.number)) {
-    console.error("invalid query number passed.");
-    res.status("404").end();
+    console.error('invalid query number passed.');
+    res.status('404').end();
     return;
   }
 
-  res.sendFile(path.join(__dirname, '../data', req.params.number, 'mattheson', (req.params.name+'.json')), function(err) {
-    if (err) {
-      console.error(err);
-      res.status("404").end();
-      return;
-    }
-  });
+  let annotationListPath = [
+    'db/apps/probstuecke-digital',
+    req.params.number,
+    'mattheson',
+    (req.params.name+'.json')
+  ].join('/');
+
+  axios.get(`http://${existConfig.host}:${existConfig.port}/exist/rest/${annotationListPath}`)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(e => {
+      console.error(e);
+    });
 }
 
 iiif.get('/:number/list/:name', sendAnnotationList);
