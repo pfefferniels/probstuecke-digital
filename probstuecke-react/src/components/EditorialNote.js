9fef2d773e50166554abd314c16e56951d6de64d
pfefferniels <niels.pfeffer@gmail.com>
2020-06-24 22:34:47 +0200

    do not use innerHTML

--- probstuecke-react/src/components/EditorialNote.js
+++ probstuecke-react/src/components/EditorialNote.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './EditorialNote.css'
 
-function EditorialNote(props) {
-  const type = props.teiNote.getAttribute('type')
-  return (
-    <div className={ type==='editorial' ? 'editorialNote'
-                                        : 'footnote'}
-         dangerouslySetInnerHTML={{__html: props.teiNote.innerHTML}}/>
-  )
+class EditorialNote extends React.Component {
+  constructor(props) {
+    super(props)
+    this.noteRef = React.createRef()
+  }
+
+  componentDidMount() {
+    this.noteRef.current.appendChild(this.props.teiNote)
+  }
+
+  render() {
+    const type = this.props.teiNote.getAttribute('type')
+    return <div className={ type==='editorial' ? 'editorialNote'
+                                               : 'footnote'}
+                ref={this.noteRef} />
+  }
 }
 
 export default EditorialNote;
