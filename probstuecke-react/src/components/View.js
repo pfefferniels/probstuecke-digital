5718b6a65bc9819107cf2e20299d150576b0cb3f
pfefferniels <niels.pfeffer@gmail.com>
2020-06-28 23:52:41 +0200

    Holzweg

--- probstuecke-react/src/components/View.js
+++ probstuecke-react/src/components/View.js
@@ -7,63 +7,36 @@ import { TOCConsumer } from './TOC'
 import Score from './Score/Score'
 import DTABf from './DTABf/DTABf'
 
-class View extends React.Component {
-  state = {
-    stavesAbove: 0,
-    stavesBelow: 0
-  }
+const View = props => {
+  const { piece } = props.match.params
 
-  componentWillMount() {
-    EventEmitter.subscribe('changeStavesAbove', (above) => {
-      this.setState(prevState => ({
-        ...prevState,
-        stavesAbove: above
-      }))
-    })
-
-    EventEmitter.subscribe('changeStavesBelow', (below) => {
-      this.setState(prevState => ({
-        ...prevState,
-        stavesBelow: below
-      }))
-    })
-  }
-
-  render() {
-    const { piece } = this.props.match.params
-
-    return (
-      <>
-        <TOCConsumer>
-          {(toc) => (
-            (!toc.ready) ? <Spinner animation='grow'/>
-                         :
-              <Tabs mountOnEnter={true}
-                    unmountOnExit={true}>
-                {Object.entries(toc.data[piece].editions).map(([key,value],i) => (
-                  <Tab key={i} eventKey={key} title={key}>
-                    <Container fluid>
-                      <Row>
-                        {value.score && <Col md={6}>
-                                          <Score key={`Score_${key}`}
-                                                 mei={value.score}
-                                                 stavesAbove={this.state.stavesAbove}
-                                                 stavesBelow={this.state.stavesBelow}/>
-                                        </Col>}
-                        {value.comments && <Col md={6}>
-                                             <DTABf key={`Text_${key}`} tei={value.comments}/>
-                                           </Col>}
-                      </Row>
-                    </Container>
-                  </Tab>
-                  ))
-                }
-              </Tabs>
-            )}
-        </TOCConsumer>
-      </>
-    )
-  }
+  return (
+    <TOCConsumer>
+      {(toc) => (
+        (!toc.ready) ? <Spinner animation='grow'/>
+                     :
+          <Tabs mountOnEnter={true}
+                unmountOnExit={true}>
+            {Object.entries(toc.data[piece].editions).map(([key,value],i) => (
+              <Tab key={i} eventKey={key} title={key}>
+                <Container fluid>
+                  <Row>
+                    {value.score && <Col md={6}>
+                                      <Score key={`Score_${key}`}
+                                             mei={value.score}/>
+                                    </Col>}
+                    {value.comments && <Col md={6}>
+                                         <DTABf key={`Text_${key}`} tei={value.comments}/>
+                                       </Col>}
+                  </Row>
+                </Container>
+              </Tab>
+              ))
+            }
+          </Tabs>
+        )}
+    </TOCConsumer>
+  )
 }
 
 export default View
