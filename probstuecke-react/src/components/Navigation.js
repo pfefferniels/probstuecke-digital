790df3e0d2700a4b14dc93494046280cdac7aabe
pfefferniels <niels.pfeffer@gmail.com>
2021-03-05 17:59:03 +0100

    fix Navbar

--- probstuecke-react/src/components/Navigation.js
+++ probstuecke-react/src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Spinner, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 import { TOC } from './TOC'
 
 const Navigation = () => {
@@ -9,35 +9,38 @@ const Navigation = () => {
 
   return (
       <Navbar>
-        <Navbar.Brand>
-          <Link to='/'>Probstücke Digital</Link>
-        </Navbar.Brand>
+        <LinkContainer to='/'>
+          <Navbar.Brand>Probstücke Digital</Navbar.Brand>
+        </LinkContainer>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href='/guidelines'>{t('editionGuidelines')}</Nav.Link>
+          <LinkContainer to='guidelines'>
+            <Nav.Link>{t('editionGuidelines')}</Nav.Link>
+          </LinkContainer>
           <NavDropdown title="Probstücke" id="basic-nav-dropdown">
             <TOC.Consumer>
               {(toc) => (
                 (!toc.ready) ? <Spinner animation='grow'/>
                              : Object.keys(toc.data).map(key => (
-                                <NavDropdown.Item>
-                                  <Link to={`/${key}`}>{key}</Link>
-                                </NavDropdown.Item>
+                                <LinkContainer to={`/${key}`}>
+                                  <NavDropdown.Item>{key}</NavDropdown.Item>
+                                </LinkContainer>
                                ))
               )}
             </TOC.Consumer>
           </NavDropdown>
           <NavDropdown title={t('indices')}>
-            <NavDropdown.Item key='indexOfPersons'>
-              <Link to='indexOfPersons'>{t('indexOfPersons')}</Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item key='bibliography'>
-              <Link to='bibliography'>{t('bibliography')}</Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item key='indexOfMusicalWorks'>
-              <Link to='indexOfMusicalWorks'>{t('indexOfMusicalWorks')}</Link>
-            </NavDropdown.Item>
+            <LinkContainer to='indexOfPersons'>
+              <NavDropdown.Item key='indexOfPersons'>{t('indexOfPersons')}</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to='bibliography'>
+              <NavDropdown.Item key='bibliography'>{t('bibliography')}</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to='indexOfMusicalWorks'>
+              <NavDropdown.Item key='indexOfMusicalWorks'>{t('indexOfMusicalWorks')}</NavDropdown.Item>
+            </LinkContainer>
           </NavDropdown>
         </Nav>
         <Form inline>
