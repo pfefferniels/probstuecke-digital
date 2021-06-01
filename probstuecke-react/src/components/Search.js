890b27595d8ac27f2bf269f309b9865cdc77a5e9
pfefferniels <niels.pfeffer@gmail.com>
2021-06-01 13:05:16 +0200

    improve search component

--- probstuecke-react/src/components/Search.js
+++ probstuecke-react/src/components/Search.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react'
 import { Spinner } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import { apiUrl } from '../config'
+import useAPIError from '../hooks/useAPIError'
 import './Search.scss'
 
 const Search = () => {
   const [results, setResults] = useState(null)
+  const [ready, setReady] = useState(false)
   const { q } = useParams()
+  const { addError } = useAPIError()
 
   useEffect(() => {
     const fetchResults = async () => {
@@ -14,23 +17,28 @@ const Search = () => {
         const data = await fetch(`${apiUrl}/search/${q}`)
         const json = await data.json()
         setResults(json.results)
+        setReady(true)
       } catch (e) {
-        console.log('error fetching search results:', e)
+        addError(`Error fetching search results: ${e}`, e)
       }
     }
 
     fetchResults()
-  }, [q])
+  }, [q, addError])
+
+  if (!ready) {
+    return <Spinner animation='grow' />
+  }
 
   if (!results) {
-    return <Spinner animation='grow' />
+    return <p>nothing found</p>
   }
 
   return (
     <div id='searchResults'>
-      {results.map((result) => {
+      {results.map((result, i) => {
         return (
-          <div className='result'>
+          <div key={`result${i}`} className='result'>
             <b className='title'>{result.title}</b>
             <p className='summary' dangerouslySetInnerHTML={{__html: result.summary}} />
           </div>
