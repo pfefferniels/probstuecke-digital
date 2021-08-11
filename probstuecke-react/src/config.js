0f3b1613fe3de7124721e9d5a3d38eb5d69f694f
Niels Pfeffer <niels.pfeffer@gmail.com>
2021-08-11 22:59:13 +0300

    assume we're running with nginx reverse proxy, add explanation for how to run locally

--- probstuecke-react/src/config.js
+++ probstuecke-react/src/config.js
@@ -1,1 +1,3 @@
-export const apiUrl = process.env.PROBSTUECKE_API || 'http://localhost:8080/exist/restxq';
+export const apiUrl = '/restxq';
+// when runnning locally, without nginx reverse proxy, use this:
+// export const apiUrl = 'http://localhost:8080/exist/restxq';
