2516f120c9f03a0017f60bc188116611af50c0e3
Niels Pfeffer <niels.pfeffer@gmail.com>
2021-08-13 12:59:03 +0300

    update NotatedMusic component

--- probstuecke-react/src/components/Text/NotatedMusic.js
+++ probstuecke-react/src/components/Text/NotatedMusic.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react'
-import { Translation } from 'react-i18next'
+import useAPIError from '../../hooks/useAPIError'
+import { useTranslation } from 'react-i18next'
 import { Alert, Spinner } from 'react-bootstrap'
 import { exampleToolkit } from '../Verovio'
 import { apiUrl } from '../../config.js'
 import './NotatedMusic.scss'
 
 const NotatedMusic = ({ teiNode, path }) => {
+  const { addError } = useAPIError()
+  const { t } = useTranslation()
+
   const [error, setError] = useState(false)
   const [svg, setSVG] = useState(null)
 
@@ -27,25 +31,18 @@ const NotatedMusic = ({ teiNode, path }) => {
         })
         exampleToolkit.loadData(text)
         const svg = exampleToolkit.renderToSVG(1)
-        //setMEIData(text)
         setSVG(svg)
       } catch (e) {
-        console.log('failed fetching MEI: ', e)
+        addError(`${t('renderError')}: ${e}`)
         setError(true)
       }
     }
 
     fetchMEI()
-  }, [])
+  }, [filename])
 
   if (error) {
-    return (
-      <Translation>
-        {(t, i18n) => (
-          <Alert>{t('renderError')}</Alert>
-        )}
-      </Translation>
-    )
+    return <Alert>{t('renderError')}</Alert>
   }
 
   if (!svg) {
